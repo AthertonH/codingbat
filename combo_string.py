14bf420e7c0332d074675d1d23f31f4c7c8e5a9d
Harrison Atherton <athertonharrison@gmail.com>
2022-08-15 02:25:07 -0700

    codebat

---
+++ combo_string.py
@@ -0,0 +1,18 @@
+# Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the
+# longer string on the inside. The strings will not be the same length, but they may be empty (length 0).
+#
+#
+# combo_string('Hello', 'hi') → 'hiHellohi'
+# combo_string('hi', 'Hello') → 'hiHellohi'
+# combo_string('aaa', 'b') → 'baaab'
+
+def combo_string(a, b):
+    a_length = len(a)
+    b_length = len(b)
+    if a_length > b_length:
+        return b + a + b
+    else:
+        return a + b + a
+
+# CODINGBAT
+# return a + b + a if len(a) < len(b) else b + a + b
