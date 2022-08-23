0697e375b8fbc1133a9463c753b621b56edf6635
Harrison Atherton <athertonharrison@gmail.com>
2022-08-22 21:14:43 -0700

    codebat

--- sum13.py
+++ sum13.py
@@ -7,7 +7,21 @@
 # sum13([1, 2, 2, 1, 13]) → 6
 
 def sum13(nums):
-    for i in range(0, len(nums), 1):
-        if i == 13:
-            x = 13
-            return sum(0,x,1)
+    sum = 0
+    for i in range(0,len(nums)):
+        if nums[i] != 13:
+            sum += nums[i]
+        else:
+            break
+    return sum
+
+def sum13(nums):
+    sum = 0
+    i = 0
+    while i < len(nums):
+        if nums[i] == 13:
+            i += 1
+        else:
+            sum += nums[i]
+        i += 1
+    return sum
